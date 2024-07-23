a0dc802e20d32ad2eb97fc49c996fb6c128351ef
0115Plugins <mygamingrust@gmail.com>
2024-07-23 02:12:04 +0100

    Template
    added template for plugins

---
+++ AutoTemplate/src/main/com/piggyplugins/StepTheTemplate/State.java
@@ -0,0 +1,12 @@
+package main.com.piggyplugins.StepTheTemplate;
+
+/*
+    State template where you add different states to your plugins
+ */
+
+public enum State {
+    ANIMATING,
+    TIMEOUT,
+    HANDLE_BREAK,
+    RESTOCK,
+}
--- src/test/java/com/piggyplugins/PluginTester.java
+++ src/test/java/com/piggyplugins/PluginTester.java
@@ -7,6 +7,8 @@ import com.piggyplugins.ItemCombiner.ItemCombinerPlugin;
 import com.piggyplugins.LeftClickBlackJack.LeftClickBlackJackPlugin;
 import com.piggyplugins.PiggyUtils.PiggyUtilsPlugin;
 import com.polyplugins.AutoBoner.AutoBonerPlugin;
+import com.polyplugins.Trapper.AutoTrapperPlugin;
+import main.com.piggyplugins.StepTheTemplate.StepTheTemplatePlugin;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 import net.runelite.client.plugins.ChinBreakHandler.ChinBreakHandlerPlugin;
@@ -18,7 +20,14 @@ public class PluginTester {
                 PiggyUtilsPlugin.class// Don't remove these
         /* Add your plugins in this method when running from the IDE.
            Make sure to include them as a dependency in the build.gradle via `testImplementation` */,
-                 AutoBonerPlugin.class, EthanPrayerFlickerPlugin.class, LeftClickBlackJackPlugin.class, ItemCombinerPlugin.class);
+                 EthanPrayerFlickerPlugin.class, LeftClickBlackJackPlugin.class,
+                ItemCombinerPlugin.class, StepTheTemplatePlugin.class
+        /*
+        okay so what ever is being pulled into this class like for example the AUTOBONER
+        if we want to remove that we just simple remove that from this list
+        same goes for adding plugins if they're here n built then they can be added e.g autoTrapper
+         */
+        );
         RuneLite.main(args);
     }
 }
